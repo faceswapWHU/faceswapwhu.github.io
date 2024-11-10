db4d0366473d3c67fe5360fa61f6b56347f33aa1
SinonShizuku <2758224213@qq.com>
2024-11-10 20:46:29 +0800

    add wsgi.py 配置gunicorn

--- frontend/composables/useApi.ts
+++ frontend/composables/useApi.ts
@@ -1,7 +1,7 @@
 // composables/useApi.ts
 import {ref} from 'vue';
 export const useApi = () => {
-  const apiBaseUrl = ref("http://127.0.0.1:5000/");
+  const apiBaseUrl = ref("http://1.94.178.239:80/");
 
   return {
     apiBaseUrl
