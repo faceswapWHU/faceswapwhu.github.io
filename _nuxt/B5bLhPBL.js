b0edf45f4f8d02372a267f84e86219a9f29e88de
SinonShizuku <SinonShizuku@users.noreply.github.com>
2024-11-10 12:47:52 +0000

    deploy: db4d0366473d3c67fe5360fa61f6b56347f33aa1

---
+++ _nuxt/B5bLhPBL.js
@@ -0,0 +1,1 @@
+import{u,af as f}from"./DjFBsM8g.js";function m(e,a){const{title:r,titleTemplate:n,...o}=e;return u({title:r,titleTemplate:n,_flatMeta:o},{...a,transform(t){const s=f({...t._flatMeta});return delete t._flatMeta,{...t,meta:s}}})}export{m as u};
