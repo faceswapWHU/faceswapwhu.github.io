9df0d2d8f7e636c02bc653a3686dcbe1d2b9d6ae
SinonShizuku <SinonShizuku@users.noreply.github.com>
2024-11-17 14:23:03 +0000

    deploy: 43e33073e6764be3406f09c3582ffd5c4c0fef90

---
+++ _nuxt/Cu3t_kiS.js
@@ -0,0 +1,1 @@
+import{u,a7 as l}from"./SB7nfnlm.js";function f(e,a){const{title:r,titleTemplate:n,...o}=e;return u({title:r,titleTemplate:n,_flatMeta:o},{...a,transform(t){const s=l({...t._flatMeta});return delete t._flatMeta,{...t,meta:s}}})}export{f as u};
